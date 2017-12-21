bdc9b3e25d885ba27607a1a69a6d78fc870973f7
colinries <colin.blaine.ries@gmail.com>
2017-12-21 06:58:39 -0800

    Added set_algebra.py

--- mlm2_18.py
+++ mlm2_18.py
@@ -13,7 +13,7 @@ def validate_base_sequence(base_sequence, RNAflag=False):
     Return True if the string base_sequence contains only upper- or lowercase
     T (or U, if RNAflag), C, A, and G characters, otherwise False
     """
-    return set(base_sequence) <= (RNAbases if RNAflag else DNAbases)  # from example 3-1
+    return set(base_sequence) <= (RNAbases if RNAflag else DNAbases) # from example 3-1
 
     # or, as in the original example,
     #
---
+++ set_algebra.py
@@ -0,0 +1,8 @@
+#!usr/bin/env python3
+# set_algebra.py - python
+# Author: Colin Ries
+#
+# Based in large part on set algebra practice problems from Richard
+
+
+de
