43481efa03611a45ef9d92ce7d84953105c1e7e5
colinries <colin.blaine.ries@gmail.com>
2017-12-21 00:03:16 -0800

    Added set_algebra.py

--- set_algebra.py
+++ set_algebra.py
@@ -2,7 +2,26 @@
 # set_algebra.py - python
 # Author: Colin Ries
 #
-# Based in large part on set algebra practice problems from Richard
+# Based in large part on set algebra practice problems from Richard Hammack's
+# 'Book of Proof' (edition 2.1) https://www.people.vcu.edu/~rhammack/BookOfProof/BookOfProof.pdf
 
 
-de
+def power_set(s):
+    """Returns the power set of set s as a list of tuples"""
+    # Credit goes to
+    p_set = [set()]
+
+    return p_set
+
+
+def cartesian_product(q,r):
+    """Returns the Cartesian product of sets q,r"""
+    pass
+
+A = set('abc')
+B = set('def')
+C = frozenset(A, B)
+print(C)
+
+
+
